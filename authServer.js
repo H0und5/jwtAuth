64b7b0215d487e9e9da15dfc96264d2f425c6773
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-11-13 13:52:38 -0600

    Setup for refresh token implementation

--- authServer.js
+++ authServer.js
@@ -8,20 +8,6 @@ const jwt = require("jsonwebtoken");
 // Create an instance of the express app
 const app = express();
 
-// Creating dummy data
-const posts = [
-  {
-    username: "Hounds",
-    title: "Versace",
-  },
-  {
-    username: "Gambhira",
-    title: "Dolce & Gabanna",
-  }
-]
-
-
-
 // Make sure app can use JSON from the body that gets passed inside a request
 app.use(express.json());
 
@@ -31,21 +17,6 @@ app.get("/posts", authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name));
 });
 
-
-// POST request through Login route for User Authentication, token
-app.post("/login", (req, res) => {
-  // Authenticate the User
-
-  const username = req.body.username;
-  const user = { name: username };
-
-  // jwt.sign(payload, secret); secret helps to serialize the token.
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-
-  // Pass our accessToken as our JSON, assuming user is authenticated. Access token will have user information saved inside of it
-  res.json({ accessToken: accessToken });
-
-})
 
 // Create middleware to authenticate token
 function authenticateToken(req, res, next) {
